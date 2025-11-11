22f7bc2542d0ba25b911f99766ce26b4ecc66539
tp <tp@octavore.com>
2025-11-11 17:01:38 -0500

    minor: CLI logs using env_logger.

--- src-tauri/src/cli/mod.rs
+++ src-tauri/src/cli/mod.rs
@@ -1,6 +1,10 @@
 pub mod commands;
 
+use std::io::Write;
+
 use clap::{Parser, Subcommand, command};
+use color_print::cwriteln;
+use log::LevelFilter;
 
 use crate::cli::commands::keychain::KeychainCommand;
 use crate::cli::commands::vault::VaultCommand;
@@ -18,6 +22,13 @@ enum AxoPassCommand {
 }
 
 pub async fn run() {
+    if std::env::var("FRITTATA_DEBUG").is_ok() || cfg!(debug_assertions) {
+        env_logger::builder()
+            .filter_level(LevelFilter::Debug)
+            .format(|buf, record| cwriteln!(buf, "<dim>{}</dim>", record.args()))
+            .init();
+    }
+
     let cli = AxoPassCli::parse();
     match cli.command {
         AxoPassCommand::Keychain(keychain) => keychain.execute().await,
