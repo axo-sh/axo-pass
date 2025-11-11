0e2340b53039eec51bf62f19fd12c485b91d5ad8
tp <tp@octavore.com>
2025-11-11 17:00:24 -0500

    minor: Rename application dir to AxoPass

--- src-tauri/src/core/dirs.rs
+++ src-tauri/src/core/dirs.rs
@@ -1,7 +1,7 @@
 use std::path::PathBuf;
 
 pub fn app_data_dir() -> PathBuf {
-    dirs::data_dir().unwrap().join("Axo Pass")
+    dirs::data_dir().unwrap().join("AxoPass")
 }
 
 pub fn vaults_dir() -> PathBuf {
