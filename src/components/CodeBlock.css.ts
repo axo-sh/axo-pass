1ca7755e3a3c244d29b0fcd8029d85eae2167010
tp <tp@octavore.com>
2025-11-15 10:10:36 -0500

    minor: CodeBlock component.

---
+++ src/components/CodeBlock.css.ts
@@ -0,0 +1,33 @@
+import {globalStyle, style} from '@vanilla-extract/css';
+
+import {vars} from '@/App.css';
+import {colorVar} from '@/styles/colors.css';
+import {spacing} from '@/styles/utils';
+
+export const codeBlockPre = style({
+  position: 'relative',
+  whiteSpace: 'pre-wrap',
+  padding: spacing(1, 1.25),
+  background: colorVar.light10,
+  borderRadius: 6,
+});
+
+export const codeBlockPreCode = style({
+  fontFamily: vars.fonts.monospace,
+  fontSize: vars.scale.sm,
+  fontWeight: 500,
+});
+
+export const codeBlockCopy = style({
+  position: 'absolute',
+  top: spacing(0.5),
+  right: spacing(0.5),
+  display: 'none',
+  ':disabled': {
+    display: 'block',
+  },
+});
+
+globalStyle(`${codeBlockPre}:hover ${codeBlockCopy}`, {
+  display: 'block',
+});
---
+++ src/components/CodeBlock.tsx
@@ -0,0 +1,50 @@
+import React, {useRef} from 'react';
+
+import {IconCheck, IconCopy} from '@tabler/icons-react';
+import {writeText} from '@tauri-apps/plugin-clipboard-manager';
+import cx from 'classnames';
+import {toast} from 'sonner';
+
+import {button} from '@/components/Button.css';
+import {codeBlockCopy, codeBlockPre, codeBlockPreCode} from '@/components/CodeBlock.css';
+
+type Props = {
+  children: React.ReactNode;
+  canCopy?: boolean;
+};
+
+export const CodeBlock: React.FC<Props> = ({canCopy, children}) => {
+  const codeRef = useRef<HTMLElement | null>(null);
+  const [copied, setCopied] = React.useState(false);
+  const onCopyClick = () => {
+    const content = codeRef.current?.innerText;
+    if (content) {
+      writeText(content);
+      toast.success('Copied to clipboard');
+      setCopied(true);
+      setTimeout(() => {
+        setCopied(false);
+      }, 1000);
+    }
+  };
+
+  const CopyIcon = copied ? IconCheck : IconCopy;
+
+  return (
+    <pre className={codeBlockPre}>
+      {canCopy && (
+        <button
+          className={cx(button({variant: 'clear', size: 'iconSmall'}), codeBlockCopy)}
+          disabled={copied}
+          onClick={onCopyClick}
+          aria-label="Copy to clipboard"
+        >
+          <CopyIcon size={16} />
+        </button>
+      )}
+      <code ref={codeRef} className={codeBlockPreCode}>
+        {children}
+      </code>
+    </pre>
+  );
+};
