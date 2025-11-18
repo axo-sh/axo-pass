728ace787396b1ad9653da11708a7acb7906bbaf
tp <tp@octavore.com>
2025-11-18 17:40:15 -0500

    fix: Add space to app data dir.

--- src-tauri/src/core/dirs.rs
+++ src-tauri/src/core/dirs.rs
@@ -1,7 +1,7 @@
 use std::path::PathBuf;
 
 pub fn app_data_dir() -> PathBuf {
-    dirs::data_dir().unwrap().join("AxoPass")
+    dirs::data_dir().unwrap().join("Axo Pass")
 }
 
 pub fn vaults_dir() -> PathBuf {
